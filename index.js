85227d4dc4c8befa221cf98bcb98e32274d62b01
Dee <deepakdilse@gmail.com>
2023-10-19 22:04:48 -0700

    add code to generate md and write to file

--- index.js
+++ index.js
@@ -1,13 +1,106 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = [
+    {
+        type: "input",
+        message: "Enter Project Title :",
+        name: "title",
+        default: "Project Title",
+    },
+    {
+        type: "input",
+        message: "Enter Project Description :",
+        name: "description",
+        default: "Project description..",
+    },
+    {
+        type: "input",
+        message: "Enter Deployed link :",
+        name: "deployedLink",
+        default: "Deployed Link..",
+    },
+    // {
+    //     type: "confirm",
+    //     message: "Create Table Of Contents section ?",
+    //     name: "tableOfContents",
+    //     default: false,
+    // },
+    {
+        type: "input",
+        message: "Enter Installation Section text :",
+        name: "installation",
+        default: "Installation instructions..",
+    },
+    {
+        type: "input",
+        message: "Enter Usage Section text :",
+        name: "usage",
+        default: "Usage...",
+    },
+    {
+        type: "confirm",
+        message: "Add license ?",
+        name: "addLicense",
+        default: true,
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Select your license :',
+        choices: [
+          'Red',
+          'Green',
+          'Blue',
+          'Yellow',
+          'Orange',
+          'Purple',
+        ],
+        when: (answer) => answer.addLicense === true,
+    },
+    {
+        type: "input",
+        message: "Enter Contribution Guidelines :",
+        name: "contribution",
+        default: "Contribution Guidelines",
+    },
+    {
+        type: "input",
+        message: "Enter Test instructions :",
+        name: "test",
+        default: "Test Instructions",
+    },
+]
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if(err) {
+        throw(err);
+    }
+    console.log(`${fileName}  created successfully.`);
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+//* use inquirer to display questions to user
+//* call generateMarkdown funcn on the data collected
+//* take what is returned, then pass it to the writeToFile function
+
+    console.log("init in index: ");
+    inquirer.prompt(questions)
+        .then(answers => {
+            console.log("answers ", answers);
+
+            //checking the answer.section generate markdowns..
+            console.log(generateMarkdown(answers));
+            writeToFile("README.md",generateMarkdown(answers));
+        });
+
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,90 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// should have a couple license options
+function renderLicenseBadge(license) {
+//   console.log("renderLicenseBadge ", license);
+//   return `## Description 
+// ${license.description}  `;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  //* return a string that has the link to selected license
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  //* create a conditional to check if license exists
+  //* call renderlicenseBadge and renderlicenseLink
+  //* use template literal to format license section and call functions
+  //* return template literal
+
+}
+
+//genearte title
+
+function generateTitle(data) {
+  return `# ${data.title}  `;
+}
+//genearte desc
+function generateDescription(data) {
+  return `## Description 
+${generateDeployedLink(data)}
+${data.description}  `;
+}
+//genearte deployed link
+function generateDeployedLink(data) {
+  return `[${data.title}](${data.deployedLink})  `;
+}
+//genearte instalation
+function generateInstallation(data) {
+  return `## Installation 
+${data.installation}  `;
+}
+//genearte Usage
+function generateUsage(data) {
+  return `## Usage
+${data.usage}  `;
+}
+//genearte contributions
+function generateContribution(data) {
+  return `## Contribution
+${data.contribution}  `;
+  }
+//genearte tests
+function generateTests(data) {
+  return `## Tests
+${data.test}  `;
+  }
+
+// generate author info
+function generateAuthor(data) {
+  return `## Author Info
+
+Deepak Sinha
+  
+* [Portfolio](https://dee-here.github.io/portfolio/)
+* [Github](https://github.com/dee-here)  `;
+  } 
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  //* create layout of what you markdown readme with template literals
+  //* hint - call renderlicense functions inside this function
+  console.log("generateMarkdown with ", data);
 
+  //${renderLicenseBadge(data)}
+
+  return `${generateTitle(data)}
+${generateDescription(data)}
+${generateInstallation(data)}
+${generateUsage(data)}
+${generateContribution(data)}
+${generateTests(data)}
+${generateAuthor(data)}
 `;
 }
 
